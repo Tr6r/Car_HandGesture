df8ad7257ac5bcf784788328d5bb508f6fad2f24
Cuong <tr6r20@gmail.com>
2024-01-03 21:35:22 +0700

    update file

---
+++ script.js
@@ -0,0 +1,585 @@
+
+import {
+    HandLandmarker,
+    FilesetResolver
+  } from "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.0";
+  
+
+    // getUsermedia parameters.
+  
+const DEFAULT_ROBOT_PROFILE = "RPI_BW_001";
+/**
+ * ESP_CW_001
+ * RPI_BW_001
+ * RPI_CL_001
+ * RPI_CL_002
+ * RPI_CW_001
+ * RPI_HA_001
+ * RPI_HW_001
+ * JTSN_HW_001
+ */
+const deviceNamePrefixMap = {
+  ESP_CW_001: "CoPlay",
+  RPI_BW_001: "BBC",
+};
+/**
+ * Bluetooth 서비스 및 특성 UUID
+ */
+const UART_SERVICE_UUID = "6e400001-b5a3-f393-e0a9-e50e24dcca9e";
+const UART_RX_CHARACTERISTIC_UUID = "6e400003-b5a3-f393-e0a9-e50e24dcca9e";
+const UART_TX_CHARACTERISTIC_UUID = "6e400002-b5a3-f393-e0a9-e50e24dcca9e";
+
+const {
+  pairButton,
+  sendMediaServerInfoButton,
+  openWebSocketButton,
+  stopButton,
+} = initializeDOMElements();
+let {
+  device,
+  websocket,
+  networkConfig,
+  gestureRecognizer,
+
+} = initializeVariables();
+
+
+
+function initializeDOMElements() {
+  const pairButton = document.getElementById("pairButton");
+  const sendMediaServerInfoButton = document.getElementById(
+    "sendMediaServerInfoButton"
+  );
+  const openWebSocketButton = document.getElementById("openWebSocketButton");
+  const stopButton = document.getElementById("stopButton");
+
+  return {
+    pairButton,
+    sendMediaServerInfoButton,
+    openWebSocketButton,
+    stopButton,
+  };
+}
+
+function initializeVariables() {
+  let device;
+  let websocket;
+  let networkConfig = {};
+  let gestureRecognizer;
+  let runningMode = "IMAGE";
+  let controlCommandMap = {
+    Closed_Fist: "N",
+    Open_Palm: "W",
+    Pointing_Up: "S",
+    Thumb_Up: "E",
+    Victory: "STOP",
+  };
+  let lastDirection;
+
+  return {
+    device,
+    websocket,
+    networkConfig,
+    gestureRecognizer,
+    runningMode,
+    controlCommandMap,
+    lastDirection,
+  };
+}
+
+async function bluetoothPairing() {
+  const robotSelect = document.getElementById("robotSelect");
+  const robotNameInput = document.getElementById("robotNameInput");
+
+  device = await connectToBluetoothDevice(
+    deviceNamePrefixMap[robotSelect.value] ?? undefined
+  );
+  robotNameInput.value = device.name;
+}
+
+function sendMediaServerInfo() {
+  const ssidInput = document.getElementById("ssidInput");
+  const passwordInput = document.getElementById("passwordInput");
+  const hostInput = document.getElementById("hostInput");
+  const portInput = document.getElementById("portInput");
+  const channelInput = document.getElementById("channelInput");
+  const robotSelect = document.getElementById("robotSelect");
+
+  networkConfig = {
+    ssid: ssidInput.value,
+    password: passwordInput.value,
+    host: hostInput.value,
+    port: portInput.value,
+    channel: "instant",
+    channel_name: channelInput.value,
+  };
+
+  const devicePort =
+    window.location.protocol.replace(/:$/, "") === "http"
+      ? networkConfig.port
+      : networkConfig.port - 1;
+
+  if (device) {
+    const metricData = {
+      type: "metric",
+      data: {
+        server: {
+          ssid: networkConfig.ssid,
+          password: networkConfig.password,
+          host: networkConfig.host,
+          port: devicePort,
+          path: `pang/ws/pub?channel=instant&name=${networkConfig.channel_name}&track=video&mode=bundle`,
+        },
+        profile: robotSelect.value,
+      },
+    };
+    sendMessageToDeviceOverBluetooth(JSON.stringify(metricData), device);
+  }
+}
+function handleChunk(frame) {
+  const canvasElement = document.getElementById("canvasElement");
+
+  drawVideoFrameOnCanvas(canvasElement, frame);
+  frame.close();
+}
+
+async function openWebSocket1() {
+
+  const path = `pang/ws/sub?channel=instant&name=${networkConfig.channel_name}&track=video&mode=bundle`;
+  const serverURL = `${
+    window.location.protocol.replace(/:$/, "") === "https" ? "wss" : "ws"
+  }://${networkConfig.host}:${networkConfig.port}/${path}`;
+
+  websocket = new WebSocket(serverURL);
+  websocket.binaryType = "arraybuffer";
+  websocket.onopen = () => {
+    if (device) {
+      document.addEventListener("keydown", handleKeyDown);
+      document.addEventListener("keyup", handleKeyUp);
+    }
+  };
+  displayMessage("Open Video WebSocket");
+
+  const videoDecoder = new VideoDecoder({
+    output: handleChunk,
+    error: (error) => console.error(error),
+  });
+
+  const videoDecoderConfig = {
+    codec: "avc1.42E03C",
+  };
+
+  if (!(await VideoDecoder.isConfigSupported(videoDecoderConfig))) {
+    throw new Error("VideoDecoder configuration is not supported.");
+  }
+
+  videoDecoder.configure(videoDecoderConfig);
+
+  websocket.onmessage = (e) => {
+    try {
+      if (videoDecoder.state === "configured") {
+        const encodedChunk = new EncodedVideoChunk({
+          type: "key",
+          data: e.data,
+          timestamp: e.timeStamp,
+          duration: 0,
+        });
+
+        videoDecoder.decode(encodedChunk);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  keepWebSocketAlive(websocket);
+}
+function drawVideoFrameOnCanvas(canvas, frame) {
+  console.log("drawing video frame on canvas");
+  
+  const ctx = canvas.getContext("2d");
+  if (!ctx) return;
+
+  ctx.drawImage(frame, 0, 0, canvas.width, canvas.height);
+}
+
+function openWebSocket() {
+  const videoElement = document.getElementById("videoElement");
+
+  const path = `pang/ws/sub?channel=instant&name=${networkConfig.channel_name}&track=video&mode=bundle`;
+  const serverURL = `${
+    window.location.protocol.replace(/:$/, "") === "https" ? "wss" : "ws"
+  }://${networkConfig.host}:${networkConfig.port}/${path}`;
+
+  websocket = new WebSocket(serverURL);
+  websocket.binaryType = "arraybuffer";
+  websocket.onopen = async () => {
+    if (device) {
+      await getVideoStream({
+        deviceId: device.id,
+      }).then(async (stream) => {
+        videoElement.srcObject = stream;
+
+        await createGestureRecognizer().then(() => {
+          detectHandGestureFromVideo(gestureRecognizer, stream);
+        });
+      });
+    }
+  };
+  displayMessage("Open Video WebSocket");
+  keepWebSocketAlive(websocket);
+}
+
+function stop() {
+  websocket.close();
+  disconnectFromBluetoothDevice(device);
+}
+
+async function createGestureRecognizer() {
+  const vision = await FilesetResolver.forVisionTasks(
+    "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.2/wasm"
+  );
+  gestureRecognizer = await GestureRecognizer.createFromOptions(vision, {
+    baseOptions: {
+      modelAssetPath:
+        "https://storage.googleapis.com/mediapipe-models/gesture_recognizer/gesture_recognizer/float16/1/gesture_recognizer.task",
+      delegate: "GPU",
+    },
+    runningMode: runningMode,
+  });
+}
+
+// async function detectHandGestureFromVideo(gestureRecognizer, stream) {
+//   if (!gestureRecognizer) return;
+
+//   const videoTrack = stream.getVideoTracks()[0];
+//   const capturedImage = new ImageCapture(videoTrack);
+//   while (true) {
+//     await capturedImage.grabFrame().then((imageBitmap) => {
+//       const detectedGestures = gestureRecognizer.recognize(imageBitmap);
+
+//       const {
+//         landmarks,
+//         worldLandmarks,
+//         handednesses,
+//         gestures,
+//       } = detectedGestures;
+
+//       if (gestures[0]) {
+//         const gesture = gestures[0][0].categoryName;
+
+//         if (Object.keys(controlCommandMap).includes(gesture)) {
+//           const direction = controlCommandMap[gesture];
+//           if (direction !== lastDirection) {
+//             lastDirection = direction;
+//             var cuong = "PP";
+//             const controlCommand = {
+//               type: "control",
+//               direction: cuong
+//             };
+//             if (websocket && websocket.readyState === WebSocket.OPEN) {
+//               // websocket.send(JSON.stringify(controlCommand));
+//               console.log(JSON.stringify(controlCommand))
+//               displayMessage(`Send '${direction}' command`);
+//             }
+//           }
+//         }
+//       }
+//     });
+//   }
+// }
+
+let handLandmarker = undefined;
+let runningMode = "IMAGE";
+let enableWebcamButton;
+let webcamRunning = false;
+
+const createHandLandmarker = async () => {
+  const vision = await FilesetResolver.forVisionTasks(
+    "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.0/wasm"
+  );
+  handLandmarker = await HandLandmarker.createFromOptions(vision, {
+    baseOptions: {
+      modelAssetPath: `https://storage.googleapis.com/mediapipe-models/hand_landmarker/hand_landmarker/float16/1/hand_landmarker.task`,
+      delegate: "GPU"
+    },
+    runningMode: runningMode,
+    numHands: 1
+  });
+
+};
+createHandLandmarker();
+
+const video = document.getElementById("webcam");
+const canvasElement = document.getElementById("output_canvas");
+const canvasCtx = canvasElement.getContext("2d");
+
+// Check if webcam access is supported.
+const hasGetUserMedia = () => !!navigator.mediaDevices?.getUserMedia;
+
+// If webcam supported, add event listener to button for when user
+// wants to activate it.
+if (hasGetUserMedia()) {
+  enableWebcamButton = document.getElementById("webcamButton");
+  enableWebcamButton.style.textTransform = "capitalize";
+  enableWebcamButton.addEventListener("click", enableCam);
+} else {
+  console.warn("getUserMedia() is not supported by your browser");
+}
+
+// Enable the live webcam view and start detection.
+function enableCam(event) {
+  if (!handLandmarker) {
+    console.log("Wait! objectDetector not loaded yet.");
+    return;
+  }
+
+  if (webcamRunning === true) {
+    webcamRunning = false;
+    enableWebcamButton.innerText = "Enable Camera";
+    
+  } else {
+    webcamRunning = true;
+    enableWebcamButton.innerText = "Disable Camera";
+  }
+
+  // getUsermedia parameters.
+  const constraints = {
+    video: true
+  };
+
+  // Activate the webcam stream.
+  navigator.mediaDevices.getUserMedia(constraints).then((stream) => {
+    video.srcObject = stream;
+    video.addEventListener("loadeddata", predictWebcam);
+  });
+}
+
+let lastVideoTime = -1;
+let results = undefined;
+console.log(video);
+async function predictWebcam() {
+  canvasElement.style.width = video.videoWidth;
+  canvasElement.style.height = video.videoHeight;
+  canvasElement.width = 200;
+  canvasElement.height = 200;
+  
+  // Now let's start detecting the stream.
+  if (runningMode === "IMAGE") {
+    runningMode = "VIDEO";
+    await handLandmarker.setOptions({ runningMode: "VIDEO" });
+  }
+  let startTimeMs = performance.now();
+  if (lastVideoTime !== video.currentTime) {
+    lastVideoTime = video.currentTime;
+    results = handLandmarker.detectForVideo(video, startTimeMs);
+  }
+  canvasCtx.save();
+  canvasCtx.clearRect(0, 0, canvasElement.width, canvasElement.height);
+  if (results.landmarks) {
+    for (const landmarks of results.landmarks) {
+      console.log(landmarks[8].y)
+      var Gesture ;
+//&& landmarks[8].z > landmarks[6].z && landmarks[12].z > landmarks[10].z && landmarks[16].z > landmarks[14].z && landmarks[20].z > landmarks[18].z
+      if(landmarks[8].y >  landmarks[5].y && landmarks[8].y <  landmarks[6].y && landmarks[12].y >  landmarks[9].y && landmarks[12].y <  landmarks[10].y  && landmarks[16].y >  landmarks[13].y && landmarks[16].y <  landmarks[14].y  && landmarks[20].y >  landmarks[17].y && landmarks[20].y <  landmarks[18].y   )
+      {
+        Gesture = "punch"
+          console.log("punch")
+      }
+      else if(landmarks[8].y > landmarks[5].y && landmarks[12].y > landmarks[9].y && landmarks[16].y > landmarks[13].y && landmarks[20].y > landmarks[17].y && landmarks[4].x < landmarks[2].x)
+      {
+        Gesture = "none";
+          console.log("none")
+      }
+      else if(landmarks[8].y < landmarks[6].y && landmarks[12].y > landmarks[9].y && landmarks[16].y > landmarks[13].y && landmarks[20].y > landmarks[17].y && landmarks[4].x > landmarks[2].x)
+      {
+        Gesture = "L";
+          console.log("L")
+      }
+      else if(landmarks[8].y > landmarks[5].y && landmarks[12].y > landmarks[9].y && landmarks[16].y > landmarks[13].y && landmarks[20].y < landmarks[18].y && landmarks[4].x < landmarks[2].x)
+      {
+        Gesture = "ut";
+          console.log("ut")
+      }
+      else if(landmarks[8].y < landmarks[6].y && landmarks[12].y < landmarks[10].y && landmarks[16].y < landmarks[14].y && landmarks[20].y < landmarks[18].y )
+      {
+          if(landmarks[4].x > landmarks[2].x)
+          {
+            Gesture = "5ngon";
+              console.log("5 ngon")
+          }
+          else {
+            Gesture = "4 ngon";
+            console.log("4 ngon")}
+          
+      }
+      const controlCommand = {
+                      type: "control",
+                      direction: Gesture
+                    };
+                    if (websocket && websocket.readyState === WebSocket.OPEN) {
+                      // websocket.send(JSON.stringify(controlCommand));
+                      // console.log(JSON.stringify(controlCommand))
+                      displayMessage(`Send '${Gesture}' command`);
+                    }
+      drawConnectors(canvasCtx, landmarks, HAND_CONNECTIONS, {
+        color: "#00FF00",
+        lineWidth: 3
+      });
+      drawLandmarks(canvasCtx, landmarks, { color: "#FF0000", lineWidth: 2 });
+    }
+  }
+  canvasCtx.restore();
+
+  // Call this function again to keep predicting when the browser is ready.
+  if (webcamRunning === true) {
+    window.requestAnimationFrame(predictWebcam);
+  }
+}
+async function connectToBluetoothDevice(deviceNamePrefix) {
+  const options = {
+    filters: [
+      { namePrefix: deviceNamePrefix },
+      { services: [UART_SERVICE_UUID] },
+    ].filter(Boolean),
+  };
+
+  try {
+    device = await navigator.bluetooth.requestDevice(options);
+    console.log("Found Bluetooth device: ", device);
+
+    await device.gatt?.connect();
+    console.log("Connected to GATT server");
+
+    return device;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+function disconnectFromBluetoothDevice(device) {
+  if (device.gatt?.connected) {
+    device.gatt.disconnect();
+  } else {
+    console.log("Bluetooth Device is already disconnected");
+  }
+}
+
+async function sendMessageToDeviceOverBluetooth(message, device) {
+  const MAX_MESSAGE_LENGTH = 15;
+  const messageArray = [];
+
+  // Split message into smaller chunks
+  while (message.length > 0) {
+    const chunk = message.slice(0, MAX_MESSAGE_LENGTH);
+    message = message.slice(MAX_MESSAGE_LENGTH);
+    messageArray.push(chunk);
+  }
+
+  if (messageArray.length > 1) {
+    messageArray[0] = `${messageArray[0]}#${messageArray.length}$`;
+    for (let i = 1; i < messageArray.length; i++) {
+      messageArray[i] = `${messageArray[i]}$`;
+    }
+  }
+
+  console.log("Connecting to GATT Server...");
+  const server = await device.gatt?.connect();
+
+  console.log("Getting UART Service...");
+  const service = await server?.getPrimaryService(UART_SERVICE_UUID);
+
+  console.log("Getting UART RX Characteristic...");
+  const rxCharacteristic = await service?.getCharacteristic(
+    UART_RX_CHARACTERISTIC_UUID
+  );
+
+  // Check GATT operations is ready to write
+  if (rxCharacteristic?.properties.write) {
+    // Send each chunk to the device
+    for (const chunk of messageArray) {
+      try {
+        await rxCharacteristic?.writeValue(new TextEncoder().encode(chunk));
+        console.log(`Message sent: ${chunk}`);
+      } catch (error) {
+        console.error(`Error sending message: ${error}`);
+      }
+    }
+  }
+}
+
+async function getVideoStream({
+  deviceId,
+  idealWidth,
+  idealHeight,
+  idealFrameRate,
+}) {
+  return navigator.mediaDevices.getUserMedia({
+    video: deviceId
+      ? {
+          deviceId,
+          width: { min: 200, ideal: idealWidth },
+          height: { min: 200, ideal: idealHeight },
+          frameRate: { ideal: idealFrameRate, max: 120 },
+        }
+      : true,
+  });
+}
+
+function displayMessage(messageContent) {
+  const messageView = document.getElementById("messageView");
+
+  if (typeof messageContent == "object") {
+    messageContent = JSON.stringify(messageContent);
+  }
+  messageView.innerHTML += `${messageContent}\n`;
+  messageView.scrollTop = messageView.scrollHeight;
+}
+
+function keepWebSocketAlive(webSocket, interval) {
+  const pingInterval = interval ?? 10000;
+  let pingTimer;
+
+  function sendPing() {
+    if (webSocket.readyState === WebSocket.OPEN) {
+      webSocket.send("ping");
+    }
+  }
+
+  function schedulePing() {
+    pingTimer = setInterval(sendPing, pingInterval);
+  }
+
+  function handlePong() {}
+
+  function handleWebSocketClose() {
+    clearInterval(pingTimer);
+  }
+
+  webSocket.addEventListener("open", () => {
+    schedulePing();
+  });
+
+  webSocket.addEventListener("message", (event) => {
+    if (event.data === "pong") {
+      handlePong();
+    }
+  });
+
+  webSocket.addEventListener("close", () => {
+    handleWebSocketClose();
+  });
+}
+function startBothWebSockets() {
+  // openWebSocket();   // Gọi hàm openWebSocket
+  openWebSocket1();  // Gọi hàm openWebSocket1
+}
+
+
+document.addEventListener("DOMContentLoaded", () => {
+  pairButton.addEventListener("click", bluetoothPairing);
+  sendMediaServerInfoButton.addEventListener("click", sendMediaServerInfo);
+  openWebSocketButton.addEventListener("click", startBothWebSockets);
+  stopButton.addEventListener("click", stop);
+});
+
